f179dff5b11d044357f964899e7d27fe7e37bd91
Muhammad Ali Aridho <120350392+alibarleey@users.noreply.github.com>
2023-02-12 23:10:49 +0700

    Add files via upload

---
+++ stream-jantung.py
@@ -0,0 +1,47 @@
+import pickle
+import numpy as np
+import streamlit as st 
+
+model = pickle.load(open('jantung.sav','rb'))
+
+st.title('Prediksi Penyakit Jantung')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    age = st.text_input('Umur')
+with col2:
+    sex = st.text_input('Jenis Kelamin')
+with col3:
+    cp = st.text_input('Jenis Nyeri Dada')
+with col1: 
+    trestbps = st.text_input('Tekanan Darah')
+with col2:
+    chol = st.text_input('Nilai Kolesterol')
+with col3:
+    fbs = st.text_input('Gula Darah')
+with col1:
+    restecg = st.text_input('Hasil Elektrokadiografi')
+with col2:
+    thalach = st.text_input('Detak jantung Maksimum')
+with col3:
+    exang = st.text_input('Induksi Angina')
+with col1:
+    oldpeak = st.text_input('ST Depression')
+with col2:
+    slope = st.text_input('Slope')
+with col3:
+    ca = st.text_input('Nilai CA')
+with col1:
+    thal = st.text_input('Nilai Thal')
+    
+heart_diagosis =''
+
+if st.button('Prediksi Penyakit Jantung'):
+    heart_prediction = model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+    
+    if(heart_prediction[0]==1):
+        heart_diagosis = 'Pasien Terkena Penyakit Jantung'
+    else:
+        heart_prediction = 'Pasien Tidak Terkena Penyakit Jantung'
+st.success(heart_diagosis)
